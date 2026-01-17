7734392f9956bae638bfce2d86eb61fb3befd89f
AytajMammadova306 <aytajamam@code.edu.az>
2026-01-17 22:22:11 +0400

    Initial

---
+++ Cinemastic/src/Core/Cinemastic.Domain/Entities/common/BaseAccountableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemastic.Domain.Entities
+{
+    internal abstract class BaseAccountableEntity: BaseEntity
+    {
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? Updated { get; set; }
+        public string CreatedBy { get; set; } = "admin";
+    }
+}
---
+++ Cinemastic/src/Core/Cinemastic.Domain/Entities/common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemastic.Domain.Entities
+{
+    internal abstract class BaseEntity
+    {
+        public string Id { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
