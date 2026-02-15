6497251d779c17f6100ef4e34ab6fd080fff0758
AytajMammadova306 <aytajamam@code.edu.az>
2026-02-15 22:39:15 +0400

    SidebarInAdmin

--- Cinemastic/Presentation/Cinemastic.MVC/obj/Debug/net8.0/Cinemastic.MVC.AssemblyInfo.cs
+++ Cinemastic/Presentation/Cinemastic.MVC/obj/Debug/net8.0/Cinemastic.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinemastic.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcdef96afb7a04277e4120b6609bc54a63b990f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6626f741f4a09a32e4069c10c7c0ffbe5ac3373b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinemastic.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinemastic.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
